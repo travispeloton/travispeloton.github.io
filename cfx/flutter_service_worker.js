e190386646d28fc8415906d44fa0cab1c22628ad
Travis Haagen <travis.haagen@onepeloton.com>
2024-09-11 16:12:14 -0700

    update

--- cfx/flutter_service_worker.js
+++ cfx/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "095890bba1004905e41e153f5da8abd9",
+const RESOURCES = {"flutter_bootstrap.js": "bbc1dc515839da904c31b6eaeedbbdac",
 "version.json": "ab389d02172e69813d8aa75dad7c5cc7",
 "index.html": "85590b484480950d5b696d9a31291931",
 "/": "85590b484480950d5b696d9a31291931",
-"main.dart.js": "6ce424ca11ea3a5bb52ab3f9224d3d79",
+"main.dart.js": "633d6779dc10af2a468c34019d4330bb",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
